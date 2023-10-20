99cb19ce2870869addf4ccea68e47392e9955f90
GshotWSL <giorgiobarturenes@gmail.com>
2023-10-20 15:45:00 +0200

    added usernames

--- Cima/db/schema.rb
+++ Cima/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_184414) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_133337) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "content", null: false
@@ -49,8 +49,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_184414) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "posts"
