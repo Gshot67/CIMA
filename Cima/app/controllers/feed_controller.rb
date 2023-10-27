4d8d5af7935fcedecea6c46bc876f3d51911dd98
gshot67 <52007031+Gshot67@users.noreply.github.com>
2023-10-27 04:23:22 +0200

    lng

--- Cima/app/controllers/feed_controller.rb
+++ Cima/app/controllers/feed_controller.rb
@@ -8,15 +8,24 @@ class FeedController < ApplicationController
 
     end
     def general
+      @languages = ['IT', 'EN', 'FR', 'SP']
+      @language = params[:lingua] || 'Italiano'
       @posts = Post.where(topic: 'general')
+      @posts = @posts.where(lingua: @language)
       render 'show'
     end
     def sport
+      @languages = ['IT', 'EN', 'FR', 'SP']
+      @language = params[:lingua] || 'Italiano'
       @posts = Post.where(topic: 'sport')
+      @posts = @posts.where(lingua: @language)
       render 'show'
     end
     def pop
+      @languages = ['IT', 'EN', 'FR', 'SP']
+      @language = params[:lingua] || 'Italiano'
       @posts = Post.where(topic: 'pop')
+      @posts = @posts.where(lingua: @language)
       render 'show'
     end
 
