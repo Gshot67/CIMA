871af7a28bae524fe996a336b6caaa17a753e27b
AndreaBaldiExe <baldi.1966232@studenti.uniroma1.it>
2023-10-29 06:14:20 +0100

    checkauth

--- Cima/app/models/user.rb
+++ Cima/app/models/user.rb
@@ -3,21 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:google_oauth2]
   
   # validates :username, presence: true   PARTI PER INCLUDERE USERNAME E NOME 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
-  end
-    
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-      user.email = data["email"] if user.email.blank?
-      end
+      user.password = Devise.friendly_token[0, 20]
+      user.full_name = auth.info.name # assuming the user model has a name
+      user.avatar_url = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
     end
   end
 end
--- Cima/db/schema.rb
+++ Cima/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_29_013756) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_29_044916) do
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
     t.text "content", null: false
@@ -85,8 +85,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_29_013756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "provider"
+    t.string "full_name"
     t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
